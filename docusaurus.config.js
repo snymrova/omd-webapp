b89bc356da531571414d1826b0a24b910562a932
sunny luthra <luthra.sunny@gmail.com>
2023-07-21 12:45:02 +0530

    doc init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://snymrova.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/lessons',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'snymrova', // Usually your GitHub org/user name.
-  projectName: 'snymrova.github.io', // Usually your repo name.
+  projectName: 'omd-webapp', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

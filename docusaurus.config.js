123ee19f5f60afd16a863357a4165f6bad579893
sunny luthra <luthra.sunny@gmail.com>
2024-07-26 21:23:43 +0530

    Ready for the launch v1.0.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,54 +1,65 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'OhMyDog Educational Portal',
-  tagline: 'Dogs are cool, Understanding them is supercool',
-  favicon: 'favicon.ico',
+  title: "OhMyDog Educational Portal",
+  tagline: "Dogs are cool, Understanding them is supercool",
+  favicon: "favicon.ico",
   // Set the production url of your site here
-  url: 'https://edu.ohmydog.rocks',
+  url: "https://edu.ohmydog.rocks",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'snymrova', // Usually your GitHub org/user name.
-  projectName: 'omd-webapp', // Usually your repo name.
+  organizationName: "snymrova", // Usually your GitHub org/user name.
+  projectName: "omd-webapp", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
-
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+     
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        indexBlog: false,
+        indexDocs: true,
+        docsRouteBasePath: "/",
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      }),
+    ],
+  ],
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/', // Serve the docs at the site's root
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-        
-        },
-        blog: {
-          showReadingTime: true,
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/", // Serve the docs at the site's root
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
         },
+        blog: false,
+
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -59,22 +70,22 @@ const config = {
     ({
       // Replace with your project's social card
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
         disableSwitch: false,
         respectPrefersColorScheme: false,
       },
-      
-      image: 'img/omd-logo.png',
+
+      image: "img/omd-logo.png",
       navbar: {
-        title: 'OhMyDog',
-        hideOnScroll: true
+        title: "OhMyDog",
+        hideOnScroll: true,
       },
       sidebar: {
         autoCollapseCategories: true,
       },
       footer: {
-        style: 'dark',
-      }
+        style: "dark",
+      },
     }),
 };
 
